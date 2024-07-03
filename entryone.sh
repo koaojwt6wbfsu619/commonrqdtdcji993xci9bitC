6e7ec1f10b629fa23d232d65cb0057d58f93b127
Bhakun laskaw <vishnujena@outlook.com>
2024-07-03 19:03:21 +0530

    Update entryone.sh

--- entryone.sh
+++ entryone.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-git clone https://github.com/klept0/MS-Rewards-Farmer
+sleep 10
 
+git clone https://sjwrameshbhadra0jsyw0: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@github.com/sjwrameshbhadra0jsyw0/0001-of-one.git
+
+sleep 10
+
+git clone https://haystwg717jjs9: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@github.com/haystwg717jjs9/kfcvarteacoA.git
